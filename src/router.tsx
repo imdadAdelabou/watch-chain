987591bc73c75a573bb934e61597276e46e1c187
Isaak Nicolas <isaak.nicolas@epitech.eu>
2024-04-10 22:36:43 +0200

    [+] home route, fix local storage path

--- src/router.tsx
+++ src/router.tsx
@@ -4,9 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
+import HomePage from "./views/home/HomePage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements([<Route path="/" element={<LandingPage />} />])
+  createRoutesFromElements([
+    <Route path="/" element={<LandingPage />} />,
+    <Route path="/home" element={<HomePage />} />
+  ])
 );
 
 export default router;
