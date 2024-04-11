ea003f8bf3dd8453548d306cd8a71ae9911e2402
geremysimon <60546874+champagnepapi1@users.noreply.github.com>
2024-04-11 14:38:49 +0200

    fin tache

--- src/router.tsx
+++ src/router.tsx
@@ -4,9 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
+import NftPage from "./views/NftPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements([<Route path="/" element={<LandingPage />} />])
+  createRoutesFromElements([
+    <Route path="/" element={<LandingPage />} />,
+    <Route path="/nft" element={<NftPage />} />,
+  ])
 );
 
 export default router;
